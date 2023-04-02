5203f13186c58275c68538c909f24005812bc4ec
Jiyong Kim <luke.jiyongkim@gmail.com>
2023-04-02 15:00:01 -0400

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Code your solution here
+function findMatching(driverNames, str) {
+    return driverNames.filter(names => names.toLowerCase() === str.toLowerCase()) 
+}
+function fuzzyMatch(driverNames, beginLetter) {
+    return driverNames.filter(names => names.charAt(0) === beginLetter.charAt(0))
+}
+
+function matchName(drivers, str) {
+    return drivers.filter(drivernames => drivernames.name === str)
+}
